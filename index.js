45efb8f8ccd2d1293b80e0d38c3cfec354a97683
karanhb-pixel <karan.9924304045@gmail.com>
2025-03-15 18:14:29 +0530

    jsx to js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import bookRoutes from "./routes/BookRoutes.jsx";
+import bookRoutes from "./routes/BookRoutes.js";
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
