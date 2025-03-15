6b8a211ed6d53c0eff4177ed2c54df67c4602440
karanhb-pixel <karan.9924304045@gmail.com>
2025-03-15 16:24:03 +0530

    First Commit
    First commit

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import bookRoutes from "./routes/bookRoutes.js";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+//Routes
+app.use("api/books", bookRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
